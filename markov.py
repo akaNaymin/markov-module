4b3461dcc48c39944ccba3014195d24a1ed822ea
akaNaymin <akanaymin@gmail.com>
2018-11-26 20:53:48 +0200

    Cleaned generation code

--- markov.py
+++ markov.py
@@ -5,25 +5,26 @@ import pickle
 class markov():
 
     def __init__(self, text):
-        self.model = self.make_markov(text)
+        self.model = {}
+        self.create_markov(text)
 
-    def make_markov(self, text):
-        word_chains = {}
+    def parse_line(self, line):
+        words = line.split() + ['\n']
+        word_count = len(words)
+        for idx in range(word_count - 1):
+            pair = words[idx:idx+2]
+            if pair[0] not in self.model.keys(): # new leader
+                self.model[pair[0]] = {pair[1]:1}
+            elif pair[1] not in self.model[pair[0]]: # new follwer
+                self.model[pair[0]][pair[1]] = 1
+            else: # add follower
+                self.model[pair[0]][pair[1]] += 1 
+
+    def create_markov(self, text):
         for line in text.split('\n'):
             line = line.strip()
             if len(line) > 0:
-                words = line.split() + ['\n']
-                word_count = len(words)
-                for idx in range(word_count - 1):
-                    pair = words[idx:idx+2]
-                    if pair[0] not in word_chains.keys():
-                        word_chains[pair[0]] = {pair[1]:1}
-                    elif pair[1] not in word_chains[pair[0]]:
-                        word_chains[pair[0]][pair[1]] = 1
-                    else:
-                        word_chains[pair[0]][pair[1]] += 1
-        return word_chains
-
+                self.parse_line(line)
 
     def pick_next(self, word):
         word_chains = self.model
